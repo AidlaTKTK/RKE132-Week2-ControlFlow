d208a10be006881fd275f97fc5b8537534a1d852
Markus Aidla <markus.aidla@tktk.ee>
2024-09-09 17:02:27 +0300

    Add week2 code

---
+++ UserAgeValidation/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Please enter your age:");
+
+string userAge = Console.ReadLine();
+int userAgeNum = 0;
+bool isAgeNumber = Int32.TryParse(userAge, out userAgeNum);
+
+Console.WriteLine($"Parse result {isAgeNumber}. User is {userAgeNum} years old.");
+
+if (!isAgeNumber)
+{
+    Console.WriteLine("Could not read age"); return;
+}
+else
+{
+    if (userAgeNum >= 13) Console.WriteLine("Welcome!");
+    else Console.WriteLine("Access denied.");
+}
---
+++ UserGenderValidation/Program.cs
@@ -0,0 +1,37 @@
+﻿// Oma lahendus
+
+Console.WriteLine("Please, enter your last name:");
+string userLastName = Console.ReadLine();
+
+Console.WriteLine("Please, select your gender (m/f/x)");
+char userGender = Char.Parse(Console.ReadLine());
+
+switch (userGender)
+{
+    case 'm':
+        Console.WriteLine($"Welcome, Mr. {userLastName}!");
+        break;
+    case 'f':
+        Console.WriteLine($"Welcome, Ms. {userLastName}!");
+        break;
+    case 'x':
+    default:
+        Console.WriteLine($"Welcome, Mx. {userLastName}!");
+        break;
+}
+
+// Video põhjal lahendus
+/*
+if (userGender == 'm')
+{
+    Console.WriteLine($"Welcome, Mr. {userLastName}");
+}
+else if (userGender == 'f')
+{
+    Console.WriteLine($"Welcome, Ms. {userLastName}");
+}
+else
+{
+    Console.WriteLine($"Welcome, {userLastName}");
+}
+*/
